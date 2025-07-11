bfbbfc084403f5a81338e5baf2e6b2c24fa25e64
Victor Gusev <viktor@nmmlm.com>
2025-07-11 16:01:43 +0300

    chore: migrate to Angular 20

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app/app-routing.module';
+import { AppComponent } from './app/app.component';
 
-import { AppModule } from './app/app.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(BrowserModule, AppRoutingModule)
+  ]
+}).catch(err => console.error(err));
